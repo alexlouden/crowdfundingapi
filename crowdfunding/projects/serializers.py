c1cfc831fcb8f6ee0a2482c40009567a8aab4299
Bachemedei <bronwyn.1992@gmail.com>
2020-08-22 09:33:41 +0800

    updated git ignore and added user model

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -14,7 +14,7 @@ class PledgeSerializer(serializers.Serializer):
 class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length=200)
-    description = serializers.CharField(max_length=None)
+    description = serializers.CharField(max_length=200)
     goal = serializers.IntegerField()
     image = serializers.URLField()
     is_open = serializers.BooleanField()
