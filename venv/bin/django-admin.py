e6fe62cc9aa9172cfeaec0f8d15e01509e05b53e
Bachemedei <bronwyn.1992@gmail.com>
2020-08-27 14:12:39 +0800

    removed database and venv from repo. Hopefully...

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Bronwyn/Desktop/She-Codes/Django/django_API/crowdfundingapi/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
